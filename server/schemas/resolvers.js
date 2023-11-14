82070484c0574004b7316bac3616eb93b3cf8e64
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-14 13:14:01 -0800

    resolvers, typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,74 @@
-// const {  } = require('../models');
+const { User, Chat } = require('../models');
 const { signToken, AuthenticationError } = require('../utils/auth');
 
-const resolvers = {}
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find();
+        },
+        user: async (parent, { _id }) => {
+            return User.findOne({ _id });
+        },
+        chat: async (parent, { _id }) => {
+            return Chat.findOne({ _id });
+        }
+    },
+    Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user }; 
+        },
+        addChat: async (parent, { text, user1, user2}, context) => {
+            if (context.user) {
+                const chat = await Chat.create(
+                    { $addToSet: {
+                        text: []
+                    }},
+                    { user1: user1 },
+                    { user2: user2 }
+                )
+            }
+            throwAuthenticationError;
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw AuthenticationError;
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw AuthenticationError;
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+        editUser: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: args._id},
+                    { email: args.email},
+                    { bio: args.bio },
+                    { photo: args.photo },
+                    { $addToSet: {
+                        interests: [args.interests]
+                    }},
+                    { new: true, runValidators: true },
+                )
+            }
+        },
+        addToChat: async (parent, args) => {
+            return Chat.findOneAndUpdate(
+                { _id: args._id },
+
+            )
+        }
+    }
+}
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,38 @@
 const typeDefs = `
+    type User {
+        _id: ID
+        username: String!
+        email: String!
+        password: String!
+        bio: String
+        photo: String
+        interests: [String]
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Chat {
+        _id: ID
+        text: [Object]!
+        user1: User!
+        user2: User!
+    }
+
+    type Query {
+        users: [User]
+        user(_id: ID!): User
+        chat(_id: ID!): Chat
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        editUser(_id: ID!, email: String, bio: String, photo: String, interests: Array): User
+        saveChat(_id: ID!, text: Object): Chat
+    }
 `;
 
 module.exports = typeDefs;
