1736da51b0e2231d6bc9cc94324a2d025e2fe1fd
Wgomez1987 <gomez.william.a1@gmail.com>
2023-11-15 10:44:04 -0800

    seed.js complete

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,19 +1,19 @@
 const connection = require('../config/connection');
 const { User, Chat } = require('../models');
-const users = require('./data');
+const users = require('./userData');
 
 connection.once('open', async () => {
     console.log('connected');
       // Delete the collections if they exist
-      let userCheck = await connection.db.listUsers({ name: 'users' }).toArray();
-      if (userCheck.length) {
-        await connection.dropCollection('users');
-      }
+      // let userCheck = await connection.db.listUsers({ name: 'users' }).toArray();
+      // if (userCheck.length) {
+      //   await connection.dropCollection('users');
+      // }
   
-      let chatCheck = await connection.db.listCollections({ name: 'chats' }).toArray();
-      if (thoughtCheck.length) {
-        await connection.dropCollection('chats');
-      }
+      // let chatCheck = await connection.db.listCollections({ name: 'chats' }).toArray();
+      // if (thoughtCheck.length) {
+      //   await connection.dropCollection('chats');
+      // }
 
     await User.collection.insertMany(users);
   
