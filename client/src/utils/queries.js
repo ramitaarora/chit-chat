e2051db9c44c56601fb21a3acb02845fb7474ea0
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-15 10:17:08 -0800

    queries finished

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,43 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+query Query($id: ID!) {
+    user(_id: $id) {
+      fullName
+      bio
+      interests
+      photo
+    }
+  }
+`
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      bio
+      email
+      fullName
+      interests
+      photo
+      username
+    }
+  }
+`
+
+export const QUERY_CHAT = gql`
+query chat($id: ID!) {
+  chat(_id: $id) {
+    text {
+      sender
+      textContent
+    }
+    user1 {
+      _id
+    }
+    user2 {
+      _id
+    }
+  }
+}
+`
