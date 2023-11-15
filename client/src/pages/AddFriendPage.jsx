86d312cf1dde9c2b605020733982aa6769fe6c54
[j chun] <[justinchun224@gmail.com]>
2023-11-15 13:03:44 -0800

    get ready for merge

--- client/src/pages/AddFriendPage.jsx
+++ client/src/pages/AddFriendPage.jsx
@@ -1,10 +1,10 @@
 import { useQuery } from '@apollo/client';
-import { QUERY_USER } from '../utils/queries';
+import { QUERY_USERS } from '../utils/queries';
 import Header from '../components/Header';
 
 export default function AddFriendPage() {
 
-    const { loading, data } = useQuery(QUERY_USER);
+    const { loading, data } = useQuery(QUERY_USERS);
 
     const users = data?.users || [];
 
