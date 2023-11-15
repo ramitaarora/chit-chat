e5ed56a71bed19d248f75d66dc7f80861776dd5d
[j chun] <[justinchun224@gmail.com]>
2023-11-15 14:17:59 -0800

    giving class to messages in convo box

--- client/src/components/ConvoBox.jsx
+++ client/src/components/ConvoBox.jsx
@@ -1,10 +1,15 @@
+import Auth from '../utils/auth';
+
 export default function ConvoBox({ chat }) {
 
     return (
         <section id="convoBox">
-            {chat.text.map((each) => (
-                <div>{textContent}</div>
-            ))}
+            {chat.text.map((message) => message.sender === Auth.getProfile().data._id ? (
+                    <div className="senderTxt">{message.textContent}</div>
+                ) : (
+                    <div className="receiverTxt">{message.textContent}</div>
+                )
+            )}
         </section>
     )
 }
