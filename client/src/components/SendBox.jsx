606124f1a6a4036c7a22497710e6a102895abb25
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-16 14:17:33 -0800

    added socket

--- client/src/components/SendBox.jsx
+++ client/src/components/SendBox.jsx
@@ -1,13 +1,44 @@
-export default function SendBox() {
+import { useMutation } from "@apollo/client";
+import { SAVE_MESSAGE } from '../utils/mutations';
+import { socket } from '../socket';
 
-    const handleSendMsg = () => {
-        // Use socket.io here
+export default function SendBox(chatID) {
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSendMsg = async (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        const input = document.getElementById('messageInput');
+        const message = input.value.trim();
+
+        if (message !== '') {
+            socket.emit('chat message', { user: 'User', message });
+
+            const [saveMessage] = useMutation(SAVE_MESSAGE);
+
+            try {
+                const { data } = await saveMessage({
+                    variables: {
+                        _id: chatID,
+                        sender: context.user._id,
+                        textContent: message,
+                    }
+                })
+                console.log(data)
+                input.value = '';
+                setIsLoading(false);
+            } catch (err) {
+                console.log(err)
+            }
+        }
     };
 
     return (
         <section>
-            <input type="text"></input>
-            <button onClick={handleSendMsg}>Send</button>
+            <form id="chatForm" onSubmit={handleSendMsg}>
+                <input id="messageInput" type="text"/>
+                <input type="submit"/>
+            </form>
         </section>
     )
 }
--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -5,17 +5,45 @@ import FriendHeader from '../components/FriendHeader';
 import ConvoBox from '../components/ConvoBox';
 import SendBox from '../components/SendBox';
 
+import { socket } from './socket';
+import { ConnectionState } from './components/ConnectionState';
+import { ConnectionManager } from './components/ConnectionManager';
+import { ConvoBox } from '../components/ConvoBox';
+import { SendBox } from '../components/SendBox';
+
 export default function ChatPage() {
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    const [fooEvents, setFooEvents] = useState([]);
+
+    useEffect(() => {
+        function onConnect() {
+            setIsConnected(true);
+        }
+
+        function onDisconnect() {
+            setIsConnected(false);
+        }
+
+        function onFooEvent(value) {
+            setFooEvents(previous => [...previous, value]);
+        }
+
+        socket.on('connect', onConnect);
+        socket.on('disconnect', onDisconnect);
+        socket.on('foo', onFooEvent);
+
+        return () => {
+            socket.off('connect', onConnect);
+            socket.off('disconnect', onDisconnect);
+            socket.off('foo', onFooEvent);
+        };
+    }, []);
 
     const { chatID } = useParams();
 
     const { loading, data } = useQuery(QUERY_CHAT, {
         variables: { id: chatID }
     });
-    
-    const handleSendMsg = () => {
-        // Use socket.io here
-    };
 
     if (loading) {
         return <div>Loading...</div>;
@@ -27,7 +55,7 @@ export default function ChatPage() {
             <main>
                 <FriendHeader userID={userID} />
                 <ConvoBox chat={selectedChat}/>
-                <SendBox sendMsg={handleSendMsg}/>
+                <SendBox chatID={chatID}/>
             </main>
         )
     }
