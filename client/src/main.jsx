43909998e00fc7b5e685dca0a8a173744952541d
[j chun] <[justinchun224@gmail.com]>
2023-11-16 10:22:13 -0800

    stash

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 // Here we import bootstrap for access to some additional styling
 
-import App from './App.jsx';
 import LandingPage from './pages/LandingPage';
 import ErrorPage from './pages/ErrorPage';
 import FriendProfilePage from './pages/FriendProfilePage.jsx';
