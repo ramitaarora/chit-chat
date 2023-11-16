ba251b264506479c59c0240a41bff1dfc90469fd
Ericlee1783 <ericlee1783@gmail.com>
2023-11-16 10:25:02 -0800

    merging

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
 // Here we import bootstrap for access to some additional styling
 
 import App from './App.jsx';
 import LandingPage from './pages/LandingPage';
 import ErrorPage from './pages/ErrorPage';
 import FriendProfilePage from './pages/FriendProfilePage.jsx';
+import DashboardPage from './pages/DashBoardPage.jsx';
 
 const router = createBrowserRouter([
   {
