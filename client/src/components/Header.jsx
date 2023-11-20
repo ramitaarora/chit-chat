530e9868b8765a335ac1078f6661ee705ec6eef4
[j chun] <[justinchun224@gmail.com]>
2023-11-20 08:41:12 -0800

    added header style to component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 export default function Header() {
     return (
-        <header><img id="header-img" src="../src/assets/chitchatheader.png"></img></header>
+        <header className="header"><img id="header-img" src="../src/assets/chitchatheader.png"></img></header>
     )
 }
