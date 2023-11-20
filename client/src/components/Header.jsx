06ec7119c2d2658f9d5243ded7e72fcbaad4625f
[j chun] <[justinchun224@gmail.com]>
2023-11-20 08:59:54 -0800

    edit header styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 export default function Header() {
     return (
-        <header className="header"><img id="header-img" src="../src/assets/chitchatheader.png"></img></header>
+        <header className="header"><img className="header-img" src="../src/assets/chitchatheader.png"></img></header>
     )
 }
--- client/src/pages/DashBoardPage.jsx
+++ client/src/pages/DashBoardPage.jsx
@@ -3,6 +3,7 @@ import Floatingbutton from '../components/Floatingbuttons.jsx';
 import { useQuery } from '@apollo/client';
 import { QUERY_FRIENDS, CHAT_EXISTS } from '../utils/queries.js';
 import { Link } from 'react-router-dom';
+import Header from '../components/Header';
 import Auth from '../utils/auth';
 
 function Dashboard() {
@@ -14,41 +15,84 @@ function Dashboard() {
     // Query for chat inside map function?
     // const { loading: chatLoading, data: chatData } = useQuery(CHAT_EXISTS);
 
-    return (
-        <main>
-            <section className="header">
-                <h1> <img src="/chitchatlogo.png"></img> Chit Chat <img src="/chitchatlogo.png"></img></h1>
+    if (friendsLoading && !friendsData) {
+        return (
+            <div>Loading Dashboard...</div>
+        )
+    } else if (friendsData && !friendsLoading) {
+        return (
+            <main>
+                <Header />
                 <h2>Chats</h2>
-            </section>
-            <section className="inbox-container">
-                {friendsLoading ? (
-                    <div>Loading...</div>
-                ) : (
-                    <>
-                        {
-                            friends.map((friend) => (
-                                <div key={friend._id} className="chat-preview">
-                                    <Link to={`/user/${friend._id}`}>
-                                        <section className="profile-picture">
-                                            <img src={friend.photo} alt="user-one"></img>
-                                        </section>
-                                    </Link>
-                                    <Link to={`chat/:chatID`}>
-                                        <section className="message-preview">
-                                            <h3>{friend.username}</h3>
-                                            <p>So about coding...</p>
-                                        </section>
-                                    </Link>
-                                </div>
-                            ))
-                        }
-                    </>
-                )}
-                <Link to="/users">Find other users!</Link>
-            </section>
-            <Floatingbutton />
-        </main>
-    )
+                <section className="inbox-container">
+                    {
+                        friends.map((friend) => (
+                            <div key={friend._id} className="chat-preview">
+                                <Link to={`/user/${friend._id}`}>
+                                    <section className="profile-picture">
+                                        <img src={friend.photo} alt="user-one"></img>
+                                    </section>
+                                </Link>
+                                <Link to={`chat/:chatID`}>
+                                    <section className="message-preview">
+                                        <h3>{friend.username}</h3>
+                                        <p>So about coding...</p>
+                                    </section>
+                                </Link>
+                            </div>
+                        ))
+                    }
+                    <Link to="/users">Find other users!</Link>
+                </section>
+                <Floatingbutton />
+            </main>
+        )
+    } else {
+        return (
+            <main>
+                <Header />
+                <div>Friends List is Empty</div>
+                <Link to="/users">Add friends!</Link>
+            </main>
+        )
+    }
+
+//     return (
+//         <main>
+//             <section className="header">
+//                 <h1> <img src="/chitchatlogo.png"></img> Chit Chat <img src="/chitchatlogo.png"></img></h1>
+//                 <h2>Chats</h2>
+//             </section>
+//             <section className="inbox-container">
+//                 {friendsLoading ? (
+//                     <div>Loading...</div>
+//                 ) : (
+//                     <>
+//                         {
+//                             friends.map((friend) => (
+//                                 <div key={friend._id} className="chat-preview">
+//                                     <Link to={`/user/${friend._id}`}>
+//                                         <section className="profile-picture">
+//                                             <img src={friend.photo} alt="user-one"></img>
+//                                         </section>
+//                                     </Link>
+//                                     <Link to={`chat/:chatID`}>
+//                                         <section className="message-preview">
+//                                             <h3>{friend.username}</h3>
+//                                             <p>So about coding...</p>
+//                                         </section>
+//                                     </Link>
+//                                 </div>
+//                             ))
+//                         }
+//                     </>
+//                 )}
+//                 <Link to="/users">Find other users!</Link>
+//             </section>
+//             <Floatingbutton />
+//         </main>
+//     )
+
 }
 
 export default Dashboard;
