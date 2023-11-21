313c80746928fa82a7c976806f63893aab700b6c
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-11-21 13:14:58 -0800

    profile picture on chat

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,4 +1,5 @@
 import { useRouteError } from "react-router-dom";
+import Floatingbutton from '../components/Floatingbuttons'
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -11,6 +12,7 @@ export default function ErrorPage() {
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+      <Floatingbutton/>
     </div>
   );
 }
